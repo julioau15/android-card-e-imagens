b1669b230cc43e5e6f7457d265d02e3d3490c3a4
julioau15 <julioaugustov@gmail.com>
2026-09-22 11:25:44 -0300

    adiciona estrutura inicial

--- app/src/main/java/com/aulasandroid/card_e_imagens/MainActivity.kt
+++ app/src/main/java/com/aulasandroid/card_e_imagens/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.aulasandroid.card_e_imagens
 
+import android.R.attr.text
+import android.R.attr.value
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aulasandroid.card_e_imagens.ui.theme.Card_e_imagensTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Card_e_imagensTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    IMCScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +45,87 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun IMCScreen(modifier: Modifier = Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Card_e_imagensTheme {
-        Greeting("Android")
+    Column(modifier.fillMaxSize()) {
+
+        Box(modifier = Modifier.fillMaxSize()){
+
+            Column(modifier = Modifier.fillMaxWidth()) {
+
+                // -- Header
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(160.dp)
+                        .background(colorResource(R.color.cor_app)),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.bmi),
+                        contentDescription = "icone BMI",
+                        modifier = Modifier.size(80.dp)
+                            .padding(vertical = 16.dp)
+                    )
+
+                    Text("Calculadora IMC")
+                }
+
+                // -- Formulario
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp)
+                        .background(Color.Red),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Seus dados",
+                        Modifier
+                            .padding(20.dp),
+                    )
+
+                    // -- Input altura
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        placeholder = {
+                            Text("Altura")
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp)
+                    )
+
+                    // -- Input Peso
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        placeholder = {
+                            Text("Peso")
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp)
+                    )
+
+                    // -- Botão de calcualr
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp)
+                    ) {
+                        Text("Calcular")
+                    }
+                }
+
+
+
+            }
+
+        }
+
     }
+
 }
